1f49389862adfe18f210728616423518fa8449d1
paolanataliaconrado-prog <paolanataliaconrado@cobaqroo.edu.mx>
2026-03-26 23:57:47 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,264 @@
+﻿<?php
+     echo "Aprendí que mientras PHP es el del proceso, como tú con tu ex, JavaScript, es el destino final, como el actual novio de tu ex y que en conjunto permiten la existencia de una aplicación web completa. Algo que considero muy interesante es que mientras PHP se enfoca, en lo que yo considero difícil como conectar con bases de datos, JavaScript muestra red flags, alertas y mensajes, es decir, da respuesta a las acciones del usuario.";
+?>
+
+<!Doctype html>
+<html>
+   <head>
+       
+       <link rel="stylesheet" href="css/style.css">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-widht,initial-scale=1.0">
+       <title> Algo sobre mí </title>
+
+            <script>
+               alert("Nunca fuiste suficiente para tu ex, tal como PHP. Ahora miras deprimido como el nuevo novio de tu ex, JavaScript es lo que tú nunca pudiste ser: el destino final.");
+            </script>
+
+       <link rel="stylesheet" href="css/style.css">
+       <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+   </head>
+
+    
+       <body> 
+           
+           <img src="Imagenes/amocira.png" width="100%" height="auto"/>
+           
+            <h1>Algo sobre mí</h1>
+           
+            <h6> Paola Natalia Conrado Cano </h6>
+            
+            <br>
+           
+           <!-- IMAGEN espaldas-->
+           <img src="Imagenes/espaldas.jpeg" width="300" height="400"/>
+           
+                   
+
+           <section>
+                <div class="Habilidades">
+            <br>        
+                     <h2> Mis habilidades </h2>
+            
+                <ul>
+                    <p>
+                    <li>Ortografía.</li>
+                    </p>
+                    
+                    <p>
+                    <li>Investigación.</li> 
+                    </p>
+                    
+                    <p>
+                    <li>Redacción.</li>
+                    </p>
+                    
+                    <p>
+                    <li>Escucha activa.</li> 
+                    </p>
+                    
+                    <p>
+                    <li>Inteligencia emocional</li>  
+                    </p>   
+                    
+                </ul>
+                    
+               </div>    
+           </section>
+           
+            <br>
+           
+           <section>
+                <div class="Hobbies"> 
+                     <h2> Mis hobbies </h2>
+               
+                <table> 
+                      <tr>
+                          <th>Mis hobbies</th>
+                          <th>Días</th>
+                          <th>Hora</th>
+                      </tr>  
+                          
+                       <tr>
+                           <td>Pasar tiempo con mi gata</td>
+                           <td>Lunes</td> 
+                           <td>9:00</td>
+                       </tr>
+                        
+                       <tr>
+                           <td>Escuchar música</td>
+                           <td>Martes</td>
+                           <td>10:00</td>
+                       </tr>
+                    
+                       <tr>
+                           <td>Escuchar podcast</td>
+                           <td>Miércoles</td>
+                           <td>23:00</td>
+                       </tr>
+                       
+                       <tr>
+                           <td>Cantar</td>
+                           <td>Jueves</td>
+                           <td>10:00</td>
+                       </tr>
+                       
+                       <tr>
+                           <td>Hacer tarea</td>
+                           <td>Viernes</td>
+                           <td>22:30</td>
+                       </tr>
+                       
+                       <tr>
+                           <td>Tomar clases en línea</td>
+                           <td>Sábado</td>
+                           <td>9:00</td>
+                       </tr>
+                       
+                       <tr>
+                           <td>Coro</td>
+                           <td>Domingo</td>
+                           <td>11:00</td>
+                       </tr>
+                       
+                  </table>      
+                    
+                </div>   
+           </section>
+         
+            <br>
+         
+          <section>
+             <div class="Deb">
+                  <h2>Mis debilidades y fortalezas</h2>
+                  
+                 <p class="Deb1">
+                       Desilución.
+                   </p>
+                 
+                   <p class="Deb2">
+                       Trabajar desde la motivación y no la disciplina.
+                   </p>
+                 
+                   <p class="Deb3">
+                        Organización poco eficiente.
+                   </p>
+              </div>     
+                   
+              
+              <div class="Fort">
+                     
+                         <p class="Deb4">
+                           Curiosidad.
+                         </p>   
+                           
+                         <p class="Deb5">
+                           Honestidad.
+                         </p>     
+                         
+                         <p class="Deb6">
+                          Gratitud.
+                         </p>
+                           
+                             
+                             
+                         <p class="Deb7">
+                             Resiliencia.
+                         </p>   
+                             
+                             
+                         <p class="Deb8">
+                             Ética de trabajo.
+                         </p>   
+                             
+              </div>
+         </section>
+                    
+            <br>    
+     
+        <section>
+             <div class="HistoriaAcadémica">
+                  <h2>Historia académica</h2>
+             </div>
+            
+            <table text-align="center">
+                 
+               
+                 <td><div class="card text-bg-success mb-3" style="width: 18rem;">
+                  <img src="Imagenes/primaria.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Primaria Victor Hugo Bolañoz Martínez</h5>
+                    <p class="card-text">Entre a la primaria a los 7 años. Recuerdo que despidieron a un muy buen maestro que me dio en primer grado y parte de segundo. Si buscan el nombre de la escuela seguramente aparezca alguna noticia relacionada..</p>
+                    <a href="https://cuartoscuro.com/fotos/individual/546229/149348" class="btn btn-warning">Noticia del despido</a>
+ 
+                    </div>
+ 
+                     </div>
+                </td> 
+                
+                <br>
+                
+                <td><div class="card text-bg-warning mb-3" style="width: 18rem;">
+                 <img src="Imagenes/secundaria.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Secundaria Juan de la Barrera</h5>
+                    <p class="card-text">Estudié en el turno matutino y tuve uno de los mjores promedios de mi generación.</p>
+                    <a href="https://sige.seq.gob.mx/portal/section/detalle_escuela.php?clavect=23DES0034R&turno=1" class="btn btn-danger">Información pública</a>
+ 
+                    </div>
+                    
+                    </div>     
+                </td>    
+                    
+                <br>
+                
+                <td><div class="card text-bg-info mb-3" style="width: 18rem;">
+                 <img src="Imagenes/bacho.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Colegio de Bachilleres Plantel Cancún Dos</h5>
+                    <p class="card-text">Estoy en sexto semestre, llevo capacitación en tics y el maestro que imparte mi capacitación, Edward, es muy buen maestro.Recuerden, Bacho la casa de los jaguares.</p>
+                    <a href="https://www.facebook.com/cobachdoscancun.bachodos/?locale=es_LA" class="btn btn-primary">Facebook de Bacho</a>
+  
+                    </div>
+                    
+                    </div>
+                </td>    
+        
+                <br>
+            
+            </table>  
+            
+            <br>        
+            
+                <p>Entré al kinder Revolución a los 5 años y dos años después, a los 7, entré a la primaria Victor Hugo Bolaños <br> Martínez. Durante esta etapa escolar recibí varios diplomas gracias a mi desempeño académico, asimismo, en<br> quinto grado participé en la etapa local de las olimpiadas del conocimiento; al año siguiente formé parte de la <br> escolta hasta que llegó la pandemia. A los 12 años entré a la secundaria Juan de la Barrera y en mi último año <br> de secundaria participé en un concurso interno de oratoria, al ganar el primer lugar di el discurso de graduación <br> para mi generación. 
+                   Decidí que mi próxima casa de estudios sería Bachilleres Dos. Ahora soy parte de la familia <br> jaguares que se graduará dentro de unos meses. </p>
+            
+             
+        </section>   
+       
+                <br>     
+           
+                 <h3>El amor de mi vida</h3>
+                <div>
+               <video src="video/cira.mp4" "width=640" height="360" controls></video>
+                </div>       
+               
+                <br>
+           
+              <h3>Playlist "Puppies"</h3>
+           
+               <p>Mira este video en YouTube
+               <a href="https://youtube.com/playlist?list=PLgdzqJjJ3Af5IlC-MDFd5HwaBS5d23wPo&si=3AL2M_gDK77BOnD2"> Haz click aquí para ver la playlist. Have Fun
+               </a>
+               </p>
+    
+          <!-- IMAGEN de carrusel--> 
+           <img src="Imagenes/carrusel.jpg" width="100%" height="auto">
+      </body>
+
+
+
+</html>
